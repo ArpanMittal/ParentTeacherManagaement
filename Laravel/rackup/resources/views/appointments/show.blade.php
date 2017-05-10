c852f2206508d93262c5dbdcf17c5ba8a098aa85
Pratyushi Mishra <Pratyushi Mishra>
2017-05-10 18:24:39 +0530

    appointment scheduler changes

--- Laravel/rackup/resources/views/appointments/show.blade.php
+++ Laravel/rackup/resources/views/appointments/show.blade.php
@@ -66,10 +66,12 @@
                     <p class="form-control-static">{{$appointmentDetails['status']}}</p>
                 </div>
                 <a class="btn btn-default" href="{{ route('appointments.index') }}">Index</a>
-                @if($appointmentDetails['status']=="Awaited")
+                @if(($appointmentDetail['requestedBy']=="Parent") &&
+                $appointmentDetails['status']=="Awaited"))
                     <a class="btn btn-success" href="{{ route('getConfirm',$appointmentDetails['requestId'])}}">Confirm Appointment</a>
                 @endif
-                @if($appointmentDetails['status']=="Confirmed" || $appointmentDetails['status']=="Awaited")
+                @if(($appointmentDetail['requestedBy']=="Parent") &&
+                ($appointmentDetails['status']=="Confirmed" || $appointmentDetails['status']=="Awaited"))
                     <a class="btn btn-danger" href="{{ route('getCancel',$appointmentDetails['requestId'])}}">Cancel Appointment</a>
                 @endif
             </form>
