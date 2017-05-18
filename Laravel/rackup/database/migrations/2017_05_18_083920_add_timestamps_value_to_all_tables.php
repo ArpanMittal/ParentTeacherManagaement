b644a531ce0578238048375412440038ff370371
Pratyushi Mishra <Pratyushi Mishra>
2017-05-18 16:54:46 +0530

    upload image controller

--- Laravel/rackup/database/migrations/2017_05_18_083920_add_timestamps_value_to_all_tables.php
+++ Laravel/rackup/database/migrations/2017_05_18_083920_add_timestamps_value_to_all_tables.php
@@ -19,104 +19,116 @@ class AddTimestampsValueToAllTables extends Migration
             
         });
         Schema::table('appointmentRequests', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('calendar_events', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('calendar_events', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('categories', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('categories', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('contents', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('contents', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('content_grade', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('content_grade', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('content_types', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('content_types', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('grades', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('grades', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('grade_school', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('grade_school', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('grade_user', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('grade_user', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('image_students', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('image_students', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('roles', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('roles', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('schools', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('schools', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('students', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('students', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
 //        Schema::table('student_user', function(Blueprint $table) {
 //            $table->dropColumn('timestamps');
@@ -132,24 +144,27 @@ class AddTimestampsValueToAllTables extends Migration
             $table->dropColumn('updated_at');
         });
         Schema::table('teacherAppointmentsSlots', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('userDetails', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('userDetails', function(Blueprint $table) {
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
         Schema::table('users', function(Blueprint $table) {
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
         });
         Schema::table('users', function(Blueprint $table){
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
 
     }
@@ -163,91 +178,91 @@ class AddTimestampsValueToAllTables extends Migration
     {
         Schema::table('appointmentRequests', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('appointmentRequests', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('calendar_events', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('calendar_events', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('categories', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('categories', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('contents', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('contents', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('content_grade', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('content_grade', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('content_types', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('content_types', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('grades', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('grades', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('grade_school', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('grade_school', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('grade_user', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('grade_user', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('image_students', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('image_students', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('roles', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('roles', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('schools', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('schools', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('students', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('students', function(Blueprint $table) {
             $table->timestamps();
@@ -261,21 +276,21 @@ class AddTimestampsValueToAllTables extends Migration
 //        });
         Schema::table('teacherAppointmentsSlots', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('teacherAppointmentsSlots', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('userDetails', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('userDetails', function(Blueprint $table) {
             $table->timestamps();
         });
         Schema::table('users', function(Blueprint $table) {
             $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+//            $table->dropColumn('updated_at');
         });
         Schema::table('users', function(Blueprint $table) {
             $table->timestamps();
