10d7f4636617fc0ddad33a6fa791d19498fabd0a
Pratyushi Mishra <Pratyushi Mishra>
2017-05-19 11:01:57 +0530

    view changes

--- Laravel/rackup/resources/views/layouts/navigation.blade.php
+++ Laravel/rackup/resources/views/layouts/navigation.blade.php
@@ -4,13 +4,16 @@
         @if($user->role_id==1)
             <div class="container">
                 <ul class="nav nav-tabs" role="tablist">
-                    <li><a href="{{ route('registerParent')}}">Register Parent</a></li>
-                    <li><a href="{{ route('registerTeacher')}}">Register Teacher</a></li>
+                    <li><a href="{{ route('registerParent')}}">Register User</a></li>
+                    <li><a href="{{ route('calendar')}}">Slots and School Events</a></li>
+                    <li><a href="{{ route('editProfileDetails')}}">Edit Profile</a></li>
+                    {{--<li><a href="{{ route('registerParent')}}">Register Parent</a></li>--}}
+                    {{--<li><a href="{{ route('registerTeacher')}}">Register Teacher</a></li>--}}
                     {{--<li><a href="{{ route('showAppointmentDetails')}}">Show Appointment Details</a></li>--}}
                     {{--<li><a href="{{ route('insertAppointmentsSlots')}}">Insert Slots</a></li>--}}
-                    <li><a href="{{ route('teachersList')}}">Teachers  List</a></li>
-                    <li><a href="{{ route('parentsList')}}">Parents List</a></li>
-                    <li><a href="{{ route('calendar')}}">Slots and School Events</a></li>
+                    {{--<li><a href="{{ route('teachersList')}}">Teachers  List</a></li>--}}
+                    {{--<li><a href="{{ route('parentsList')}}">Parents List</a></li>--}}
+
                 </ul>
             </div>
         @endif
@@ -19,8 +22,11 @@
             <div class="container">
                 <ul class="nav nav-tabs" role="tablist">
                     {{--<li><a href="{{ route('upload')}}">Upload File</a></li>--}}
-                    <li><a href="{{ route('uploadLink')}}">Upload Link</a></li>
+                    {{--<li><a href="{{ route('uploadLink')}}">Upload Link</a></li>--}}
+                    <li><a href="{{ route('upload.index')}}">Upload Links</a></li>
+                    <li><a href="{{ route('uploadFile')}}">Upload Image</a></li>
                     <li><a href="{{ route('appointments.index')}}">View Appointments</a></li>
+                    <li><a href="{{ route('editProfileDetails')}}">Edit Profile</a></li>
                     {{--<li><a href="{{route('teacherAppointments')}}">Show Appointments</a></li>--}}
                 </ul>
             </div>
