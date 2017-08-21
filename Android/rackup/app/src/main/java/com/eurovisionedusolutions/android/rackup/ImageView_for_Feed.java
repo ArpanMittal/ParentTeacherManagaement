fc553e82275874d7fac957370c41a366f8544c07
Arpan <arpanmittal123@gmail.com>
2017-08-21 13:30:33 +0530

    test

--- Android/rackup/app/src/main/java/com/eurovisionedusolutions/android/rackup/ImageView_for_Feed.java
+++ Android/rackup/app/src/main/java/com/eurovisionedusolutions/android/rackup/ImageView_for_Feed.java
@@ -1,5 +1,6 @@
 package com.eurovisionedusolutions.android.rackup;
 
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
